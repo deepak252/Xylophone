a2003c5bedd8376b0b4f2bb9b6985af7541c5bf2
deepak <trojanhorsedc@gmail.com>
2021-04-28 18:41:38 +0530

    Final Project(Completed)

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,45 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  
+  void playSound(int noteNumber)
+  {
+    final player=AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+  Widget colorNote({int noteNumber,Color color})
+  {
+    return Expanded(
+            child: FlatButton(
+              onPressed: (){playSound(noteNumber);},
+              color: color,
+            ),
+          );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body:SafeArea(
-          child:Center(
-            child: FlatButton(
-              onPressed: (){
-                // static AudioCache player=new AudioCache();
-                final player=AudioCache();
-                // static AudioCache player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text("Click me")
-            )
-          )
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              colorNote(noteNumber: 1,color: Colors.red),
+              colorNote(noteNumber: 2,color: Colors.orange),
+              colorNote(noteNumber: 3,color: Colors.yellow),
+              colorNote(noteNumber: 4,color: Colors.lightGreen),
+              colorNote(noteNumber: 5,color: Colors.green),
+              colorNote(noteNumber: 6,color: Colors.blue),
+              colorNote(noteNumber: 7,color: Colors.purple),
+
+
+            ],
+          )          
         )
 
       )      
     );
   }
 }
+
+
